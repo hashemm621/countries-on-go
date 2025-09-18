35033e9cb25b7225fddbed59e4d9b25a21e71f62
md-hashem <hashemm_621@hotmail.com>
2025-09-19 01:10:37 +0600

    setup project inverionment with all nassery link and file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './Components/Countries'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>World On The Go .....</h1>
+     <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>I want to go Bangladesh</h1>
+        </div>
+    );
+};
+
+export default Countries;
